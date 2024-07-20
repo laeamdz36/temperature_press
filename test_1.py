da4583202589e6ef8245cf7eb059ee8811b8c94e
Luis P <luis3mendez6@gmail.com>
2024-07-20 00:05:52 -0600

    correction on fucntion info

--- test_1.py
+++ test_1.py
@@ -3,11 +3,8 @@
 print("Hola mundo")
 
 
-def printing_info()
+def printing_info():
+    """Print info for the CLI in the raspberry"""
 
-
-"""Print info for the CLI in the raspberry"""
-
-msg = "Hola Mundo"
-
-print(msg * 10)
+    msg = "Hola Mundo"
+    print(msg * 10)
