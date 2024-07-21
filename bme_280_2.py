f178ec6642ea7266cc92eefdacbbdf9143dd1461
Luis <luis3mendez6@gmail.com>
2024-07-21 09:29:09 -0600

    adding timestamp to out

--- bme_280_2.py
+++ bme_280_2.py
@@ -11,8 +11,9 @@ bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus, address)
 
 bme280_data = bme280.sample(bus, address)
+time_stamp = f"Tiempo: {bme280_data.timestamp}"
 humidity = f"Humedad actual: {bme280_data.humidity} %"
 pressure = f"Presion actual: {bme280_data.pressure} hPa"
 ambient_temperature = f"Temperatura: {bme280_data.temperature} °C"
 
-print(humidity, pressure, ambient_temperature, sep="\n")
+print(time_stamp, humidity, pressure, ambient_temperature, sep="\n")
