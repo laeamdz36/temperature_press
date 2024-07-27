e9f2d269545577de10e8d879de066088cb805a84
Luis <luis3mendez6@gmail.com>
2024-07-27 01:10:45 -0600

    modify to full path for python scr

--- run_app.sh
+++ run_app.sh
@@ -2,4 +2,4 @@
 
 set -e
 source "/home/lmdz_366/Documents/Temperature_pressure/temp_venv/bin/activate"
-python -u insert_mariadb.py
+python -u /home/lmdz_366/Documents/Temperature_pressure/insert_mariadb.py
