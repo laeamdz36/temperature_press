437ad8c56811e8adf831bfc3fa24d5b5b432cf44
Luis <luis3mendez6@gmail.com>
2024-07-28 09:47:11 -0600

    modify key name for gcp

--- google_bg.py
+++ google_bg.py
@@ -7,15 +7,15 @@ import pandas as pd
 
 def build_clinet():
     """Build client from service account"""
-    path_key = "/home/lmdz_366/Documents/gcp_key.json"
+    path_key = "/home/lmdz_366/Documents/gcp_key_2.json"
     client = bigquery.Client.from_service_account_json(path_key)
 
     return client
 
 
 def test_query(client):
-
-    table_id = "chrome-parity-419400.temperature_valle_azul.data_2024_1"
+    """Testing query to bigquery"""
+    # table_id = "chrome-parity-419400.temperature_valle_azul.data_2024_1"
     bg_query = "SELECT * FROM `chrome-parity-419400.lpm366.table_1` LIMIT 100"
     result = client.query(bg_query)
     df = result.to_dataframe()
@@ -23,5 +23,5 @@ def test_query(client):
 
 
 if __name__ == "__main__":
-    client = build_clinet()
-    test_query(client)
+    client_bq = build_clinet()
+    test_query(client_bq)
