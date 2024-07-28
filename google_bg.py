aa4b9187e2bd538b3ede8258f4b14295102fbdf0
Luis <luis3mendez6@gmail.com>
2024-07-28 08:46:37 -0600

    add gcp client build

--- google_bg.py
+++ google_bg.py
@@ -1,3 +1,14 @@
-"""Docstring"""
+"""Module for insert into google bigquey data"""
+# Place the json keys into a different directory
 
 from google.cloud import bigquery
+
+
+def build_clinet():
+    """Build client from service account"""
+    path_key = "/home/lmdz_366/Documents/gcp_key.json"
+    cliente = bigquery.Client.from_service_account_json(path_key)
+
+
+if __name__ == "__main__":
+    build_clinet()
